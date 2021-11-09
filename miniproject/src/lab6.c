178b2099917f419799139432ed68e5ffcaa576e0
Jack <myers395@purdue.edu>
2021-11-09 13:12:05 -0500

    Whoops forgot lab 6 code

---
+++ miniproject/src/lab6.c
@@ -0,0 +1,181 @@
+
+//=============================================================================
+// ECE 362 lab experiment 6 -- Analog Input/Output and DMA
+//=============================================================================
+
+#include "stm32f0xx.h"
+#include <string.h> // for memset() declaration
+#include <math.h>   // for MA_PI
+
+// Be sure to change this to your login...
+const char login[] = "myers395";
+
+
+//===========================================================================
+// 2.1 Configuring GPIO
+//===========================================================================
+void enable_ports(void) {
+}
+
+//===========================================================================
+// 2.2 SPI Output one 7 segment display
+//===========================================================================
+uint8_t waveTableIndex = 0x00;
+extern const char font[];
+
+//===========================================================================
+// 2.3 Debouncing a Keypad
+//===========================================================================
+const char keymap[] = "DCBA#9630852*741";
+uint8_t hist[16];
+uint8_t col;
+char queue[2];
+int qin;
+int qout;
+
+int read_buttonpress(uint16_t *addr, int n)
+{
+    //addr IDR for gpio port
+    //bit of gpio port that button is on
+
+    return *addr & (0x1 << n);
+}
+
+void push_queue(int n) {
+    n = (n & 0xff) | 0x80;
+    queue[qin] = n;
+    qin ^= 1;
+}
+
+uint8_t pop_queue() {
+    uint8_t tmp = queue[qout] & 0x7f;
+    queue[qout] = 0;
+    qout ^= 1;
+    return tmp;
+}
+
+void update_history(int c, int rows)
+{
+    for(int i = 0; i < 4; i++) {
+        hist[4*c+i] = (hist[4*c+i]<<1) + ((rows>>i)&1);
+        if (hist[4*c+i] == 1)
+          push_queue(4*c+i);
+    }
+}
+
+char get_buttonpress() {
+    for(;;) {
+        asm volatile ("wfi" : :);   // wait for an interrupt
+        if (queue[qout] == 0)
+            continue;
+        return keymap[pop_queue()];
+    }
+}
+
+//===========================================================================
+// 2.5 Output Sine Wave
+//===========================================================================
+#define N 1000
+#define RATE 20000
+short int wavetable[N];
+int step = 0;
+int offset = 0;
+int volume = 2048;
+
+void init_wavetable(void) {
+    for(int i=0; i < N; i++){
+        wavetable[i] = 32767 * sin(2 * M_PI * i / N);
+    }
+}
+
+void set_freq(float f) {
+    step = f * N / RATE * (1<<16);
+}
+
+void setup_dac() {
+    RCC->AHBENR |= RCC_AHBENR_GPIOAEN;
+    GPIOA->MODER |= GPIO_MODER_MODER4;
+    RCC->APB1ENR |= RCC_APB1ENR_DACEN;
+    DAC->CR &= ~DAC_CR_EN1;
+    DAC->CR &= ~DAC_CR_BOFF1;
+    DAC->CR |= DAC_CR_TEN1;
+    DAC->CR |= DAC_CR_TSEL1;
+    DAC->CR |= DAC_CR_EN1;
+}
+
+void init_tim7(void) {
+    RCC->APB1ENR |= RCC_APB1ENR_TIM7EN;
+    TIM7->PSC = 47;
+    int countup = (1000000 / RATE) - 1;
+    TIM7->ARR = countup;
+    TIM7->CR1 |= TIM_CR1_CEN;
+    TIM7->DIER |= TIM_DIER_UIE;
+    TIM7->CR1 |= TIM_CR1_CEN;
+    NVIC->ISER[0] |= 1 << TIM7_IRQn;
+}
+
+void TIM7_IRQHandler(void) {
+    DAC->SWTRIGR |= DAC_SWTRIGR_SWTRIG1;
+    TIM7->SR &= ~TIM_SR_UIF;
+    offset += step;
+    if (offset>>16 > N){
+        offset -= N<<16;
+    }
+    int sample = wavetable[offset>>16];
+    sample *= volume;
+    sample = sample>>16;
+    sample += 2048;
+    DAC->DHR12R1 = sample;
+}
+
+
+//===========================================================================
+// Main and supporting functions
+//===========================================================================
+int main(void)
+{
+    init_wavetable();
+    set_freq(150);
+    setup_dac();
+    init_tim7();
+
+}
+
+const char font[] = {
+    0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01,
+    0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01,
+    0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01,
+    0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01,
+    0x00, // 32: space
+    0x86, // 33: exclamation
+    0x22, // 34: double quote
+    0x76, // 35: octothorpe
+    0x00, // dollar
+    0x00, // percent
+    0x00, // ampersand
+    0x20, // 39: single quote
+    0x39, // 40: open paren
+    0x0f, // 41: close paren
+    0x49, // 42: asterisk
+    0x00, // plus
+    0x10, // 44: comma
+    0x40, // 45: minus
+    0x80, // 46: period
+    0x00, // slash
+    // digits
+    0x3f, 0x06, 0x5b, 0x4f, 0x66, 0x6d, 0x7d, 0x07, 0x7f, 0x67,
+    // seven unknown
+    0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01,
+    // Uppercase
+    0x77, 0x7c, 0x39, 0x5e, 0x79, 0x71, 0x6f, 0x76, 0x30, 0x1e, 0x00, 0x38, 0x00,
+    0x37, 0x3f, 0x73, 0x7b, 0x31, 0x6d, 0x78, 0x3e, 0x00, 0x00, 0x00, 0x6e, 0x00,
+    0x39, // 91: open square bracket
+    0x00, // backslash
+    0x0f, // 93: close square bracket
+    0x00, // circumflex
+    0x08, // 95: underscore
+    0x20, // 96: backquote
+    // Lowercase
+    0x5f, 0x7c, 0x58, 0x5e, 0x79, 0x71, 0x6f, 0x74, 0x10, 0x0e, 0x00, 0x30, 0x00,
+    0x54, 0x5c, 0x73, 0x7b, 0x50, 0x6d, 0x78, 0x1c, 0x00, 0x00, 0x00, 0x6e, 0x00
+};
