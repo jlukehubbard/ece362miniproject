8a42adf6f3e8cab13f242106013788c853ef8ced
Jack <myers395@purdue.edu>
2021-12-05 19:00:51 -0500

    revert to Dec 4th

--- miniproject/CMSIS/device/stm32f0xx.h
+++ miniproject/CMSIS/device/stm32f0xx.h
@@ -75,7 +75,7 @@
   /* #define STM32F070xB */   
   /* #define STM32F042 */
   /* #define STM32F070x6 */   
-  #define STM32F091
+  /* #define STM32F091 */
   /* #define STM32F030xC */  
 #endif /* STM32F030 || STM32F031 || STM32F051 || STM32F072 || STM32F042 || STM32F091 ||
           STM32F070xB || STM32F070x6 || STM32F030xC */
@@ -118,7 +118,7 @@
    In this case, these drivers will not be included and the application code will 
    be based on direct access to peripherals registers 
    */
-  #define USE_STDPERIPH_DRIVER
+  /*#define USE_STDPERIPH_DRIVER*/
 #endif /* USE_STDPERIPH_DRIVER */
 
 /**
